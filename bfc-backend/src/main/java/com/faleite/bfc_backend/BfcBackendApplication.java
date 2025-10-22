4b7818eb4ab308218e0ce4027cbfb7a86ef142a8
faleite <fabricio_2310@hotmail.com>
2025-10-22 20:57:52 +0100

    (Update) first commit

---
+++ bfc-backend/src/main/java/com/faleite/bfc_backend/BfcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.bfc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfcBackendApplication.class, args);
+	}
+
+}
---
+++ bfc-backend/src/test/java/com/faleite/bfc_backend/BfcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faleite.bfc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
