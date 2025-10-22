8960b8df2ab961a8b76dcdec7156efb53bed75a8
faleite <fabricio_2310@hotmail.com>
2025-10-22 21:39:05 +0100

    (Update) rename project

---
+++ backend/src/main/java/com/faleite/finance_sitter/FinanceSitterApplication.java
@@ -0,0 +1,13 @@
+package com.faleite.finance_sitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSitterApplication.class, args);
+	}
+
+}
